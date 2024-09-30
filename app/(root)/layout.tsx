86c817de04f5efa7b06cdf65307ca9d92009ca6e
solletivivek <2100031220@kluniversity.in>
2024-09-30 22:05:36 +0530

    implement intial files and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>home</div>
+    </div>
+  )
+}
+
+export default page
